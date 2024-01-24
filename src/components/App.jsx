0ca2ad025a489c97f198b482064f1debe06918ce
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-01-24 09:05:50 +0200

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,53 @@
-// import { Reader } from './Reader/Reader';
-// import { LoginForm } from './LoginForm/LoginForm';
-// import { Button } from './Button/Button';
-// import { useState } from 'react';
-// import data from '../data/news.json';
-// import './App.css';
-// import { LangSwitcher } from './LangSwitcher/LangSwitcher';
-
+import { Reader } from './Reader/Reader';
+import { LoginForm } from './LoginForm/LoginForm';
+import { Button } from './Button/Button';
+import { useState } from 'react';
+import data from '../data/news.json';
+import './App.css';
+import { LangSwitcher } from './LangSwitcher/LangSwitcher';
 import { FeedBackForm } from './FeedBackForm/FeedBackForm';
 
-// const App = () => {
-//   const [signIn, setSignIn] = useState(true);
-//   const [posts, setPosts] = useState(false);
-//   const [searchBar, setSearchBar] = useState(false);
-//   const [lang, setLang] = useState('uk');
-
-//   const handleSignIn = () => {
-//     setSignIn(true);
-//     setPosts(false);
-//     setSearchBar(false);
-//   };
-
-//   const handlePosts = () => {
-//     setSignIn(false);
-//     setSearchBar(false);
-//     setPosts(true);
-//   };
-
-//   const handleLangSwitcher = () => {
-//     setSignIn(false);
-//     setSearchBar(true);
-//     setPosts(false);
-//   };
-
-//   // Колбек-функція для обробки сабміту форми
-//   const handleLogin = userData => {
-//     // Виконуємо необхідні операції з даними
-//     console.log(userData);
-//   };
-
-//   return (
-//     <div className="app">
-//       <div className="btns">
-//         <Button onClick={handleSignIn}>Sign in</Button>
-//         <Button onClick={handlePosts}>Posts</Button>
-//         <Button onClick={handleLangSwitcher}>Lang Switcher</Button>
-//       </div>
-
-//       {signIn && <LoginForm onSubmit={handleLogin} />}
-//       {posts && <Reader data={data} lang={lang} />}
-//       {searchBar && <LangSwitcher lang={lang} onSelect={setLang} />}
-//     </div>
-//   );
-// };
-
 const App = () => {
+  const [lang, setLang] = useState('uk');
+  const [content, setContent] = useState(null);
+
+  const handleClick = cont => {
+    setContent(cont);
+  };
+
+  const handleLogin = userData => {
+    console.log(userData);
+  };
+
   return (
     <div className="app">
-      <FeedBackForm></FeedBackForm>
+      <div className="btns">
+        <Button
+          onClick={() => {
+            handleClick(<LoginForm onSubmit={handleLogin} />);
+          }}
+        >
+          Sign in
+        </Button>
+        <Button
+          onClick={() => {
+            handleClick(<Reader data={data} lang={lang} />);
+          }}
+        >
+          Posts
+        </Button>
+        <Button
+          onClick={() =>
+            handleClick(<LangSwitcher lang={lang} onSelect={setLang} />)
+          }
+        >
+          Lang Switcher
+        </Button>
+        <Button onClick={() => handleClick(<FeedBackForm />)}>
+          FeedBackForm
+        </Button>
+      </div>
+      {content ? content : <p>Empty</p>}
     </div>
   );
 };
